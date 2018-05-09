ce199c34af1098e902e27b9005289a4704719d5d
ruqoyah <rukayatodukoya123@gmail.com>
2018-05-09 15:29:45 +0100

    ch(get-projects): get projects from database
    This commit gets all the projects in
    the database.
    
    [Finishes #157431002]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import seedProjects from '../seeders/projectSeeder';
+import getProjects from '../controllers/projectController';
 
 const router = express.Router();
+
+router.get('/api/projects', getProjects);
 
 router.get('/api/projects/seed', seedProjects);
 
